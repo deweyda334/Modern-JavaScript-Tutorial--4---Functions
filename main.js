19a9df03a2c58f740b79ba52de89cef960491bb5
deweyda334 <153093259+deweyda334@users.noreply.github.com>
2024-05-10 14:16:16 -0400

    Updated the arrow function script.

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+// function expression
+// const speak = function(name, time = 'night') {
+//     console.log(`Good ${time}, ${name}!`);
+// };
+
+
+
+
+const speak = (name,time='night') => `Good ${time}, ${name}!`;
+console.log(speak('Alexander'));
+
+// function greet(){
+//     console.log('hey there which school do you attend')
+// }
+
+
+
+
+
+
+
+
